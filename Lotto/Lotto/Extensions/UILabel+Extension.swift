a52c79e5c7a74ca257e63950ea2846e7cb2ad1d0
Hminchae <hminchae@gmail.com>
2024-06-05 21:06:01 +0900

    Day 17 Assignment : 로또  API로 정보 받아 화면에 구현

---
+++ Lotto/Lotto/Extensions/UILabel+Extension.swift
@@ -0,0 +1,40 @@
+//
+//  UILabel+Extension.swift
+//  Lotto
+//
+//  Created by 황민채 on 6/5/24.
+//
+
+import UIKit
+
+extension UILabel {
+    func setBallUI(_ text: Int) {
+        self.text = String(text)
+        self.textColor = .white
+        self.font = .boldSystemFont(ofSize: 20)
+        self.clipsToBounds = true
+        self.layer.cornerRadius = 15
+        self.textAlignment = .center
+        
+        switch text {
+        case 1...9:
+            self.backgroundColor = #colorLiteral(red: 0.9923834205, green: 0.7945680022, blue: 0.002898791805, alpha: 1)
+        case 10...19:
+            self.backgroundColor = #colorLiteral(red: 0.4120162725, green: 0.7748999596, blue: 0.9445131421, alpha: 1)
+        case 20...29:
+            self.backgroundColor = #colorLiteral(red: 0.9982922673, green: 0.4414684772, blue: 0.4473998547, alpha: 1)
+        case 30...45:
+            self.backgroundColor = #colorLiteral(red: 0.6588235497, green: 0.6588235497, blue: 0.6588235497, alpha: 1)
+        default:
+            self.backgroundColor = .white
+        }
+    }
+    
+    func setPlusLabelUI() {
+        self.text = "+"
+        self.textColor = .label
+        self.textAlignment = .center
+        self.font = .boldSystemFont(ofSize: 20)
+        self.backgroundColor = .clear
+    }
+}
--- Lotto/Lotto/ViewController/LookUpLottoViewController.swift
+++ Lotto/Lotto/ViewController/LookUpLottoViewController.swift
@@ -7,76 +7,146 @@
 
 import UIKit
 
+import Alamofire
 import SnapKit
 
 class LookUpLottoViewController: UIViewController, UITextFieldDelegate {
     
     let lottoRoundTextField = UITextField()
     
+    // 당첨번호 안내 label
     let infoLabel = {
         let label = UILabel()
         label.textColor = .label
         label.text = "당첨번호 안내"
+        label.font = .systemFont(ofSize: 15)
+        return label
+    }()
+    
+    // ex) 2020-08-20 추첨
+    let infoOfRoundLabel = UILabel()
+    
+    let separaterView = UIView()
+    
+    let roundStack = UIStackView()
+    // 회차
+    var keyRound: Int = 986
+    // ~ 회차
+    let roundLabel = UILabel()
+    
+    // 당첨결과
+    let resultLabel = {
+        let label = UILabel()
+        label.text = "당첨결과"
+        label.font = .boldSystemFont(ofSize: 20)
+        label.textColor = .label
         
         return label
     }()
     
-    let infoOfRoundLabel = {
-        let label = UILabel()
-        label.textColor = .lightGray
-    }
+    let ballStack = UIStackView()
     
-    let separaterView = UIView()
+    var stackItem1 = UILabel()
+    var stackItem2 = UILabel()
+    var stackItem3 = UILabel()
+    var stackItem4 = UILabel()
+    var stackItem5 = UILabel()
+    var stackItem6 = UILabel()
+    var stackItem7 = UILabel()
     
-    // ~ 회차
-    let winningLabel = UILabel()
-    // 당첨결과
-    let resultLabel = {
-        let label = UILabel()
-        label.font = .boldSystemFont(ofSize: 20)
-        label.textColor = UIColor.random()
-        return label
-    }()
-    
-    let stackView = UIStackView()
-    
-    let stackItem1 = UILabel()
-    let stackItem2 = UILabel()
-    let stackItem3 = UILabel()
-    let stackItem4 = UILabel()
-    let stackItem5 = UILabel()
-    let stackItem6 = UILabel()
-    let stackItem7 = UILabel()
-    let stackItemPlus = UILabel()
+    var stackItemPlus = UILabel()
     
     let pickerView = UIPickerView()
     
     let numbers = Array(1...986).reversed() as [Int]
     
-    let keyRound: Int = 986
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureHierachy()
         
         lottoRoundTextField.inputView = pickerView
+        
         pickerView.delegate = self
         pickerView.dataSource = self
         lottoRoundTextField.delegate = self
 
         configureLayout()
         configureUI()
+        configureNetwork()
+        configureRoundStack()
+        configureBallStack()
     }
     
     func configureHierachy() {
         view.addSubview(lottoRoundTextField)
+        view.addSubview(infoLabel)
+        view.addSubview(infoOfRoundLabel)
+        view.addSubview(separaterView)
+        view.addSubview(roundStack)
+        view.addSubview(ballStack)
+    }
+    
+    func configureRoundStack() {
+        roundStack.translatesAutoresizingMaskIntoConstraints = false
+        roundStack.axis = .horizontal
+        roundStack.spacing = 0
+        roundStack.distribution = .equalCentering
+        roundStack.isLayoutMarginsRelativeArrangement = true
+        
+        roundStack.addArrangedSubview(roundLabel)
+        roundStack.addArrangedSubview(resultLabel)
+    }
+    
+    func configureBallStack() {
+        ballStack.translatesAutoresizingMaskIntoConstraints = false
+        ballStack.axis = .horizontal
+        ballStack.spacing = 10
+        ballStack.distribution = .fillEqually
+        ballStack.isLayoutMarginsRelativeArrangement = true
+        
+        ballStack.addArrangedSubview(stackItem1)
+        ballStack.addArrangedSubview(stackItem2)
+        ballStack.addArrangedSubview(stackItem3)
+        ballStack.addArrangedSubview(stackItem4)
+        ballStack.addArrangedSubview(stackItem5)
+        ballStack.addArrangedSubview(stackItem6)
+        ballStack.addArrangedSubview(stackItemPlus)
+        ballStack.addArrangedSubview(stackItem7)
     }
     
     func configureLayout() {
         lottoRoundTextField.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+            make.top.equalTo(view.safeAreaLayoutGuide)
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
-            make.height.equalTo(30)
+            make.height.equalTo(45)
+        }
+        
+        infoLabel.snp.makeConstraints { make in
+            make.top.equalTo(lottoRoundTextField.snp_bottomMargin).offset(20)
+            make.leading.equalTo(view.safeAreaLayoutGuide).offset(20)
+        }
+        
+        infoOfRoundLabel.snp.makeConstraints { make in
+            make.top.equalTo(lottoRoundTextField.snp_bottomMargin).offset(20)
+            make.trailing.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+        separaterView.snp.makeConstraints { make in
+            make.top.equalTo(infoOfRoundLabel.snp_bottomMargin).offset(20)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+            make.height.equalTo(0.5)
+        }
+        
+        roundStack.snp.makeConstraints { make in
+            make.top.equalTo(separaterView.snp_bottomMargin).offset(20)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(120)
+        }
+        
+        ballStack.snp.makeConstraints { make in
+            make.top.equalTo(roundStack.snp_bottomMargin).offset(20)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+            make.height.equalTo(33)
         }
     }
     
@@ -84,6 +154,39 @@ class LookUpLottoViewController: UIViewController, UITextFieldDelegate {
         lottoRoundTextField.borderStyle = .roundedRect
         lottoRoundTextField.layer.cornerRadius = 10
         lottoRoundTextField.tintColor = .gray
+        lottoRoundTextField.textAlignment = .center
+        
+        infoOfRoundLabel.font = .systemFont(ofSize: 13)
+        infoOfRoundLabel.textColor = .darkGray
+        
+        separaterView.backgroundColor = .lightGray
+        
+        roundLabel.text = "\(keyRound)회"
+        roundLabel.font = .boldSystemFont(ofSize: 20)
+        roundLabel.textColor = UIColor.random()
+    }
+    
+    func configureNetwork() {
+        
+        let url = "\(APIURL.lottoURL)\(keyRound)"
+        
+        AF.request(url).responseDecodable(of: Lotto.self) { response in
+            switch response.result {
+            case .success(let value):
+                print(value.lottoDate)
+                self.infoOfRoundLabel.text = "\(value.lottoDate) 추첨"
+                self.stackItem1.setBallUI(value.num1)
+                self.stackItem2.setBallUI(value.num2)
+                self.stackItem3.setBallUI(value.num3)
+                self.stackItem4.setBallUI(value.num4)
+                self.stackItem5.setBallUI(value.num5)
+                self.stackItem6.setBallUI(value.num6)
+                self.stackItem7.setBallUI(value.num7)
+                self.stackItemPlus.setPlusLabelUI()
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 }
 
@@ -97,11 +200,41 @@ extension LookUpLottoViewController : UIPickerViewDelegate, UIPickerViewDataSour
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        winningLabel.text = "\(numbers[row])회차"
+        keyRound = numbers[row]
+        roundLabel.text = "\(numbers[row])회"
         lottoRoundTextField.text = "\(numbers[row])회차"
+        configureNetwork()
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return "\(numbers[row])회차"
+    }
+}
+
+struct Lotto: Decodable {
+    let returnValue : String
+    let lottoDate : String
+    let num1 : Int
+    let num2 : Int
+    let num3 : Int
+    let num4 : Int
+    let num5 : Int
+    let num6 : Int
+    let num7 : Int
+    let prizeMoney : Int
+    let lottoRound : Int
+    
+    enum CodingKeys: String, CodingKey {
+        case returnValue = "returnValue"
+        case lottoDate = "drwNoDate"
+        case num1 = "drwtNo1"
+        case num2 = "drwtNo2"
+        case num3 = "drwtNo3"
+        case num4 = "drwtNo4"
+        case num5 = "drwtNo5"
+        case num6 = "drwtNo6"
+        case num7 = "bnusNo"
+        case prizeMoney = "firstWinamnt"
+        case lottoRound = "drwNo"
     }
 }
