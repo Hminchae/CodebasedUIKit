ed96acf0ed65ee51077d5223764e2d9bedd429fd
Hminchae <hminchae@gmail.com>
2024-06-05 12:43:46 +0900

    Spotify Clone : Welcome, Auth 구현

--- Spotify/Spotify/Resources/AppDelegate.swift
+++ Spotify/Spotify/Resources/AppDelegate.swift
@@ -25,6 +25,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         window.makeKeyAndVisible()
         self.window = window
+        
         return true
     }
 
@@ -41,7 +42,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
