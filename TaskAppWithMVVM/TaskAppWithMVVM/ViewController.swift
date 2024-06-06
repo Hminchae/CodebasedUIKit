96f529c0375cb28805e802b2500243ff606a1215
Hminchae <hminchae@gmail.com>
2024-06-06 17:01:22 +0900

    TaskApp : 시드 파일 생성

---
+++ TaskAppWithMVVM/TaskAppWithMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskAppWithMVVM
+//
+//  Created by 황민채 on 6/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
