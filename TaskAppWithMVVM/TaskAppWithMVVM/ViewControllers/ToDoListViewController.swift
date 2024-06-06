b1b31757d0093372b85df0c614b566299f160eaa
Hminchae <hminchae@gmail.com>
2024-06-06 18:14:52 +0900

    TaskApp : MVVM을 위한 폴더 구조 정리

--- TaskAppWithMVVM/TaskAppWithMVVM/ViewControllers/ToDoListViewController.swift
+++ TaskAppWithMVVM/TaskAppWithMVVM/ViewControllers/ToDoListViewController.swift
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ToDoListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        title = "Thanky's Tasks"
+
     }
-
-
 }
 
