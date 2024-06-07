e7819604c5b7a0acfa7f91fa7c69131c15cebb8f
Hminchae <hminchae@gmail.com>
2024-06-07 12:05:11 +0900

    Holidays Assignment : 다마고찌 시드파일 생성 및 에셋 추가

---
+++ Tamagotchi/Tamagotchi/TamagotchisViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TamagotchisViewController.swift
+//  Tamagotchi
+//
+//  Created by 황민채 on 6/7/24.
+//
+
+import UIKit
+
+class TamagotchisViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 0.9602500796, green: 0.989430964, blue: 0.9885126948, alpha: 1)
+    }
+}
+
