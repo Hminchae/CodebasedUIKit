4b033b001f19dbb2054be4c94b784f6216f680e0
Hminchae <hminchae@gmail.com>
2024-06-09 01:48:48 +0900

    Holiday Assignment : 다마고찌 밥먹기/물먹기 기능 구현
    [완료되지 못한 부분]
    - 다마고찌 한번에 먹을 수 있는 양 제한
    - 다마고찌 이미지 바로바로 렌더링

--- Tamagotchi/Tamagotchi/DetailTamagotchiViewController.swift
+++ Tamagotchi/Tamagotchi/DetailTamagotchiViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import SnapKit
 
 class DetailTamagotchiViewController: UIViewController {
-
+    
     var tamagotchiInfo: Tamagotchi?
     let user = UserDefaultManager.shared
     
@@ -31,35 +31,262 @@ class DetailTamagotchiViewController: UIViewController {
     
     private var bubbleLabel: UILabel = {
         let v = UILabel()
-        v.font = .systemFont(ofSize: 13)
+        v.font = .boldSystemFont(ofSize: 13)
         v.textColor = #colorLiteral(red: 0.3222457469, green: 0.4339936972, blue: 0.4861731529, alpha: 1)
         v.textAlignment = .center
         v.lineBreakMode = .byWordWrapping
+        v.numberOfLines = 0
         
         return v
     }()
+    
+    lazy private var TamaImageView: UIImageView = {
+        let v = UIImageView()
+        v.contentMode = .scaleAspectFit
+        switch user.tamagotchiType {
+        case 1:
+            v.image = UIImage(named: "1-\(user.tamagotchiLevel)")
+        case 2:
+            v.image = UIImage(named: "2-\(user.tamagotchiLevel)")
+        case 3:
+            v.image = UIImage(named: "3-\(user.tamagotchiLevel)")
+        default:
+            v.image = UIImage(named: "1-1")
+        }
+        return v
+    }()
+    
+    lazy private var tamaNameLabel: UILabel = {
+        let v = UILabel()
+        v.textColor = #colorLiteral(red: 0.3222457469, green: 0.4339936972, blue: 0.4861731529, alpha: 1)
+        v.font = .boldSystemFont(ofSize: 13)
+        v.layer.borderWidth = 1
+        v.layer.borderColor = UIColor.lightGray.cgColor
+        v.textAlignment = .center
+        v.clipsToBounds = true
+        v.layer.cornerRadius = 5
+        v.text = " \((tamagotchiInfo?.name)!) "
+        
+        return v
+    }()
+    
+    lazy private var levelStack: UIStackView = {
+        let v = UIStackView()
+        v.axis = .horizontal
+        v.spacing = 0
+        v.distribution = .equalSpacing
+        v.alignment = .fill
+        v.isLayoutMarginsRelativeArrangement = false
+        
+        return v
+    }()
+    
+    lazy private var levelLabel: UILabel = {
+        let v = UILabel()
+        v.font = .boldSystemFont(ofSize: 13)
+        v.text = "LV\(user.tamagotchiLevel)"
+        v.textColor = #colorLiteral(red: 0.3222457469, green: 0.4339936972, blue: 0.4861731529, alpha: 1)
+        
+        return v
+    }()
+    
+    private var firstDot: UILabel = {
+        let v = UILabel()
+        v.font = .boldSystemFont(ofSize: 13)
+        v.text = "﹒"
+        v.textColor = #colorLiteral(red: 0.3222457469, green: 0.4339936972, blue: 0.4861731529, alpha: 1)
+        
+        return v
+    }()
+    
+    lazy private var riceLabel: UILabel = {
+        let v = UILabel()
+        v.font = .boldSystemFont(ofSize: 13)
+        v.text = "밥알 \(user.rice)개"
+        v.textColor = #colorLiteral(red: 0.3222457469, green: 0.4339936972, blue: 0.4861731529, alpha: 1)
+        
+        return v
+    }()
+    
+    private let secondDot: UILabel = {
+        let v = UILabel()
+        v.font = .boldSystemFont(ofSize: 13)
+        v.text = "﹒"
+        v.textColor = #colorLiteral(red: 0.3222457469, green: 0.4339936972, blue: 0.4861731529, alpha: 1)
+        
+        return v
+    }()
+    
+    lazy private var waterDropLabel: UILabel = {
+        let v = UILabel()
+        v.font = .boldSystemFont(ofSize: 13)
+        v.text = "물방울 \(user.waterDrop)개"
+        v.textColor = #colorLiteral(red: 0.3222457469, green: 0.4339936972, blue: 0.4861731529, alpha: 1)
+        
+        return v
+    }()
+    
+    lazy private var riceTextField: UITextField = {
+        let v = UITextField()
+        v.placeholder = "밥주세용"
+        v.borderStyle = .none
+        v.textAlignment = .center
+        v.textColor = #colorLiteral(red: 0.3222457469, green: 0.4339936972, blue: 0.4861731529, alpha: 1)
+        v.tintColor = .gray
+        v.font = .boldSystemFont(ofSize: 17)
+        v.keyboardType = .numberPad
+        
+        return v
+    }()
+    
+    private var riceButton: UIButton = {
+        let v = UIButton()
+        v.setTitle("밥먹기", for: .normal)
+        v.setImage(UIImage(systemName: "drop.circle"), for: .normal)
+        v.tintColor = #colorLiteral(red: 0.3222457469, green: 0.4339936972, blue: 0.4861731529, alpha: 1)
+        v.setTitleColor(#colorLiteral(red: 0.3222457469, green: 0.4339936972, blue: 0.4861731529, alpha: 1), for: .normal)
+        v.clipsToBounds = true
+        v.layer.borderWidth = 1.5
+        v.layer.borderColor = #colorLiteral(red: 0.3222457469, green: 0.4339936972, blue: 0.4861731529, alpha: 1)
+        v.layer.cornerRadius = 8
+        v.titleLabel?.font = .boldSystemFont(ofSize: 15)
+        v.addTarget(self, action: #selector(riceButtonClicked), for: .touchUpInside)
+        return v
+    }()
+    
+    lazy private var waterTextField: UITextField = {
+        let v = UITextField()
+        v.placeholder = "물주세용"
+        v.borderStyle = .none
+        v.textAlignment = .center
+        v.textColor = #colorLiteral(red: 0.3222457469, green: 0.4339936972, blue: 0.4861731529, alpha: 1)
+        v.tintColor = .gray
+        v.font = .boldSystemFont(ofSize: 17)
+        v.keyboardType = .numberPad
+        
+        return v
+    }()
+    
+    private var waterDropButton: UIButton = {
+        let v = UIButton()
+        v.setTitle("물먹기", for: .normal)
+        v.setImage(UIImage(systemName: "leaf.circle"), for: .normal)
+        v.tintColor = #colorLiteral(red: 0.3222457469, green: 0.4339936972, blue: 0.4861731529, alpha: 1)
+        v.setTitleColor(#colorLiteral(red: 0.3222457469, green: 0.4339936972, blue: 0.4861731529, alpha: 1), for: .normal)
+        v.clipsToBounds = true
+        v.layer.borderWidth = 1.5
+        v.layer.borderColor = #colorLiteral(red: 0.3222457469, green: 0.4339936972, blue: 0.4861731529, alpha: 1)
+        v.layer.cornerRadius = 8
+        v.titleLabel?.font = .boldSystemFont(ofSize: 15)
+        v.addTarget(self, action: #selector(waterDropButtonClicked), for: .touchUpInside)
+        
+        return v
+    }()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = #colorLiteral(red: 0.9602500796, green: 0.989430964, blue: 0.9885126948, alpha: 1)
         bubbleLabel.text = tamagotchiSay.sentence.randomElement()
-        configureNavigationItem()
         setUpUI()
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        addBottomBorderToTextField(textField: riceTextField)
+        addBottomBorderToTextField(textField: waterTextField)
+    }
+    
+    private func addBottomBorderToTextField(textField: UITextField) {
+        let border = CALayer()
+        border.frame = CGRect(x: 0, y: textField.frame.size.height - 1, width: textField.frame.width, height: 1)
+        border.backgroundColor = #colorLiteral(red: 0.3222457469, green: 0.4339936972, blue: 0.4861731529, alpha: 1).cgColor
+        textField.layer.addSublayer(border)
     }
     
     func setUpUI() {
         view.addSubview(bubbleView)
+        view.addSubview(TamaImageView)
+        view.addSubview(tamaNameLabel)
+        view.addSubview(levelStack)
+        view.addSubview(riceTextField)
+        view.addSubview(riceButton)
+        view.addSubview(waterTextField)
+        view.addSubview(waterDropButton)
         
+        configureNavigationItem()
         configureBubbleUI()
+        configureLevelStackUI()
         configureLayout()
     }
     
     func configureLayout() {
         bubbleView.snp.makeConstraints { make in
-            make.top.equalTo(view.snp.top).offset(80)
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(40)
             make.centerX.equalTo(view.snp.centerX)
-            make.height.equalTo(300)
+            make.height.equalTo(150)
         }
+        
+        TamaImageView.snp.makeConstraints { make in
+            make.top.equalTo(bubbleView.snp.bottom).offset(5)
+            make.centerX.equalTo(view.snp.centerX)
+        }
+        
+        tamaNameLabel.snp.makeConstraints { make in
+            make.top.equalTo(TamaImageView.snp.bottom).offset(5)
+            make.centerX.equalTo(view.snp.centerX)
+            make.height.equalTo(25)
+        }
+        
+        levelStack.snp.makeConstraints { make in
+            make.top.equalTo(tamaNameLabel.snp.bottom).offset(10)
+            make.centerX.equalTo(view.snp.centerX)
+            make.width.equalTo(170)
+            make.height.equalTo(20)
+        }
+        
+        riceTextField.snp.makeConstraints { make in
+            make.top.equalTo(levelStack.snp.bottom).offset(40)
+            make.height.equalTo(40)
+            make.width.equalTo(200)
+            make.centerX.equalToSuperview().offset(-(100 / 2 + 10 / 2))
+        }
+        
+        riceButton.snp.makeConstraints { make in
+            make.top.equalTo(levelStack.snp.bottom).offset(40)
+            make.leading.equalTo(riceTextField.snp.trailing).offset(10)
+            make.height.equalTo(40)
+            make.width.equalTo(100)
+            make.centerX.equalToSuperview().offset(200 / 2 + 10 / 2)
+        }
+        
+        waterTextField.snp.makeConstraints { make in
+            make.top.equalTo(riceTextField.snp.bottom).offset(20)
+            make.leading.equalTo(riceTextField.snp.leading)
+            make.height.equalTo(40)
+            make.width.equalTo(200)
+        }
+        
+        waterDropButton.snp.makeConstraints { make in
+            make.top.equalTo(riceButton.snp.bottom).offset(20)
+            make.leading.equalTo(waterTextField.snp.trailing).offset(10)
+            make.height.equalTo(40)
+            make.width.equalTo(100)
+        }
+    }
+    
+    func configureLevelStackUI() {
+        print(#function)
+        levelStack.addArrangedSubview(levelLabel)
+        levelStack.addArrangedSubview(firstDot)
+        levelStack.addArrangedSubview(riceLabel)
+        levelStack.addArrangedSubview(secondDot)
+        levelStack.addArrangedSubview(waterDropLabel)
+        
+        levelLabel.sizeToFit()
+        firstDot.sizeToFit()
+        riceLabel.sizeToFit()
+        secondDot.sizeToFit()
+        waterDropLabel.sizeToFit()
     }
     
     func configureBubbleUI() {
@@ -71,8 +298,9 @@ class DetailTamagotchiViewController: UIViewController {
         }
         
         bubbleLabel.snp.makeConstraints { make in
-            make.verticalEdges.equalTo(bubbleImageView.snp.verticalEdges).inset(10)
-            make.horizontalEdges.equalTo(bubbleImageView.snp.horizontalEdges).inset(10)
+            make.verticalEdges.equalTo(bubbleImageView.snp.verticalEdges).inset(15)
+            make.centerX.equalTo(bubbleView.snp.centerX)
+            make.width.equalTo(200)
         }
     }
     
@@ -94,6 +322,29 @@ class DetailTamagotchiViewController: UIViewController {
         navigationController?.pushViewController(vc, animated: true)
     }
     
+    // TODO: 텍스트필드 49까지만, 넘으면 얼럿 띄우기
+    @objc func riceButtonClicked() {
+        if let text = riceTextField.text, !text.isEmpty {
+            if let riceToAdd = Int(text) {
+                user.rice += riceToAdd
+            }
+        } else {
+            user.rice += 1
+        }
+        riceLabel.text = "밥알 \(user.rice)개"
+    }
+    
+    @objc func waterDropButtonClicked() {
+        if let text = waterTextField.text, !text.isEmpty {
+            if let waterToAdd = Int(text) {
+                user.waterDrop += waterToAdd
+            }
+        } else {
+            user.waterDrop += 1
+            print(user.tamagotchiLevel)
+        }
+        waterDropLabel.text = "밥알 \(user.waterDrop)개"
+    }
 }
 
 
--- Tamagotchi/Tamagotchi/UserDefaultManager.swift
+++ Tamagotchi/Tamagotchi/UserDefaultManager.swift
@@ -11,9 +11,10 @@ import Foundation
  ✅ 유저가 가져야할 것
  대장님 이름
  밥알
- 물방울 -> 밥알과 물방울로 레벨 계산
+ 물방울
+ 레벨 -> 밥알과 물방울로 레벨 계산
  다마고치 종류 -> 다마고찌 종류가 미설정이면 초기설정 X
-*/
+ */
 
 class UserDefaultManager {
     static let shared = UserDefaultManager()
@@ -30,7 +31,7 @@ class UserDefaultManager {
     
     var rice : Int {
         get {
-            return UserDefaults.standard.integer(forKey: "rice") 
+            return UserDefaults.standard.integer(forKey: "rice")
         }
         
         set {
@@ -48,7 +49,39 @@ class UserDefaultManager {
         }
     }
     
-    // 유저디폴트로 저장프로퍼티 가능?
+    var tamagotchiLevel: Int {
+        let riceScore = self.rice / 5
+        let waterDropScore = self.waterDrop / 2
+        let totalScore = riceScore + waterDropScore
+        
+        switch totalScore {
+        case 0..<10:
+            return 1
+        case 10..<20:
+            return 1
+        case 20..<30:
+            return 2
+        case 30..<40:
+            return 3
+        case 40..<50:
+            return 4
+        case 50..<60:
+            return 5
+        case 60..<70:
+            return 6
+        case 70..<80:
+            return 7
+        case 80..<90:
+            return 8
+        case 90..<100:
+            return 9
+        case 10...:
+            return 10
+        default:
+            return 1
+        }
+    }
+    
     var tamagotchiType : Int {
         get {
             return UserDefaults.standard.integer(forKey: "tamagotchiType")
