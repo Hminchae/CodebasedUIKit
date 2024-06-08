05ac72f8f84a343e9ba4694bf60b9aef3b0ed452
Hminchae <hminchae@gmail.com>
2024-06-09 02:39:43 +0900

    Holiday Assignment : 폴더 구조 정리 및 파일 생성
    - 이름 설정 뷰컨트롤러 생성

---
+++ Tamagotchi/Tamagotchi/Setting/nameSettingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  nameSettingViewController.swift
+//  Tamagotchi
+//
+//  Created by 황민채 on 6/9/24.
+//
+
+import UIKit
+
+class nameSettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+}
