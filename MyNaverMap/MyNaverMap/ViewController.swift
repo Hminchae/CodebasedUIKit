96b7cfbe2fa2b3ca7341cdd768b5246f8423c588
Hminchae <hminchae@gmail.com>
2024-06-10 00:36:14 +0900

    MyNaverMap : 시드파일 생성

---
+++ MyNaverMap/MyNaverMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyNaverMap
+//
+//  Created by 황민채 on 6/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
