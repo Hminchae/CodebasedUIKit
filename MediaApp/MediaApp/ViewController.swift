8a8303618ba219c7ff57e76feb7bdbe2e440159a
Hminchae <hminchae@gmail.com>
2024-06-10 16:15:00 +0900

    Day 18 Assignment : Movie Project 시드파일 생성

---
+++ MediaApp/MediaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MediaApp
+//
+//  Created by 황민채 on 6/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
