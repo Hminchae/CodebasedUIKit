25b15bb332d5e93a484f3647e1fbe50f6d136888
Hminchae <hminchae@gmail.com>
2024-06-22 20:20:56 +0900

    aeWeather : 날씨앱 시드파일 생성

---
+++ aeWeather/aeWeather/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  aeWeather
+//
+//  Created by 황민채 on 6/22/24.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
