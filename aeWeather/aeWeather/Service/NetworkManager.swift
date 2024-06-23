06242962b191949ba4f4223e062fbeae4ade292d
Hminchae <hminchae@gmail.com>
2024-06-24 00:11:06 +0900

    aeWeather : 네트워크 매니저 구현

--- aeWeather/aeWeather/Service/NetworkManager.swift
+++ aeWeather/aeWeather/Service/NetworkManager.swift
@@ -8,26 +8,28 @@
 import Foundation
 import Alamofire
 
-//class NetworkManager {
-//    
-//    static let shared = NetworkManager()
-//    
-//    private init() { }
-//    
-//    func getLotto(_ query: String, completionHandler: @escaping (Int) -> Void) { // static 붙이는게 효율적일까? => 화면이 하나이고, 영영 메모리 공간을 차지하여도 됨(이 함수가 이 앱의 어느정도 비중이냐)
-//        let url = APIKey.baseUrl
-//        let 
-//        AF.request(url).responseDecodable(of: Lotto.self) { response in
-//            switch response.result {
-//            case .success(let value):
-//                print(value.drwNo) // 탈출 클로저
-//                print(value.self)
-//                
-//                completionHandler(value.totSellamnt)
-//            case .failure(let error):
-//                print(error)
-//            }
-//        }
-//    }
-//    
-//}
+class NetworkManager {
+    
+    static let shared = NetworkManager()
+    
+    private init() { }
+    
+    func getWeather(lat: Double, lon: Double, completionHandler: @escaping (OpenWeather) -> Void) {
+        let url = APIKey.baseUrl
+        let para: Parameters = [
+            "lat": lat,
+            "lon": lon,
+            "appid": APIKey.weatherKey,
+            "units": "metric"
+        ]
+        
+        AF.request(url, parameters: para).responseDecodable(of: OpenWeather.self) { response in
+            switch response.result {
+            case .success(let value):
+                completionHandler(value)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
