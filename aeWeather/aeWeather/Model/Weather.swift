bae0bb29dc4698b78f4c2b54faf9f5039bdd6bbd
Hminchae <hminchae@gmail.com>
2024-06-24 00:10:44 +0900

    aeWeather : 모델 구현

--- aeWeather/aeWeather/Model/Weather.swift
+++ aeWeather/aeWeather/Model/Weather.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+struct OpenWeather: Decodable {
+    let weather: [Weather]
+    let main: Main
+    let wind: Wind
+    let sys: Sys
+    let id: Int
+    let name: String
+}
+
+struct Main: Decodable {
+    let temp: Double
+    let humidity: Int
+}
+
+struct Sys: Decodable {
+    let country: String
+    let sunrise, sunset: Int
+}
+
+struct Weather: Decodable {
+    let main: String // ex) "Clear"
+    let icon: String
+}
+
+struct Wind: Decodable {
+    let speed: Double
+    let deg: Int
+}
