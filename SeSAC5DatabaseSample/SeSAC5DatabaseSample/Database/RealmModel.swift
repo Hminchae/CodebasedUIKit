c9a65ae4209bc11dbc1a3bbc3a7a2344aff82203
Hminchae <hminchae@gmail.com>
2024-07-02 11:30:53 +0900

    Day 31 Class : Realm, 메모 테이블 생성

---
+++ SeSAC5DatabaseSample/SeSAC5DatabaseSample/Database/RealmModel.swift
@@ -0,0 +1,28 @@
+//
+//  RealmModel.swift
+//  SeSAC5DatabaseSample
+//
+//  Created by 황민채 on 7/2/24.
+//
+
+import Foundation
+
+import RealmSwift
+
+class TodoTable: Object {
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var momoTitle: String // 메모제목(필수)
+    @Persisted var momoContent: String? // 메모 내용(옵션)
+    @Persisted var money: Int // 금액(필수)
+    @Persisted var category: String // 필수
+    @Persisted var resisterDate: Date // 필수
+    
+    convenience init(momoTitle: String, memoContent: String?, money: Int, category: String, resisterDate: Date) {
+        self.init()
+        self.momoTitle = momoTitle
+        self.momoContent = momoContent
+        self.money = money
+        self.category = category
+        self.resisterDate = resisterDate
+    }
+}
