efac73b637684548115020d10dd451188bc85295
Hminchae <hminchae@gmail.com>
2024-07-11 01:30:01 +0900

    iCrypto App : 네트워크 통신 구현

--- iCrypto/iCrypto/Controllers/HomeController.swift
+++ iCrypto/iCrypto/Controllers/HomeController.swift
@@ -8,13 +8,9 @@
 import UIKit
 
 class HomeController: UIViewController {
-
+    
     // MARK: - Variables
-    private let coins: [Coin] = [
-            Coin(id: 1, name: "Bitcoin", maxSupply: 200, rank: 1, pricingData: PricingData(CAD: CAD(price: 50000, market_cap: 1_000_000))),
-            Coin(id: 2, name: "Ethereum", maxSupply: nil, rank: 2, pricingData: PricingData(CAD: CAD(price: 2000, market_cap: 500_000))),
-            Coin(id: 3, name: "Monero", maxSupply: nil, rank: 3, pricingData: PricingData(CAD: CAD(price: 200, market_cap: 250_000)))
-        ]
+    private let viewModel: HomeControllerViewModel
     
     // MARK: - UI Components
     private let tableView = {
@@ -24,16 +20,51 @@ class HomeController: UIViewController {
         
         return tableView
     }()
-
+    
     // MARK: - UI Lifecycle
+    init(_ viewModel: HomeControllerViewModel = HomeControllerViewModel()) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         self.setupUI()
         
         self.tableView.delegate = self
         self.tableView.dataSource = self
         
+        self.viewModel.onCoinsUpdated = { [weak self] in
+            DispatchQueue.main.async {
+                self?.tableView.reloadData()
+            }
+        }
+        
+        self.viewModel.onErrorMessage = { [weak self] error in
+            DispatchQueue.main.async {
+                let alert = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+                
+                switch error {
+                case .serverError(let serverError):
+                    alert.title = "Server Error \(serverError.errorCode)"
+                    alert.message = serverError.errorMessage
+                case .unknown(let string):
+                    alert.title = "Error Fetching Coins"
+                    alert.message = string
+                case .decodingError(let string):
+                    alert.title = "Error Parsing Data"
+                    alert.message = string
+                }
+                
+                self?.present(alert, animated: true, completion: nil)
+            }
+        }
     }
     
     // MARK: - UI Setup
@@ -51,13 +82,13 @@ class HomeController: UIViewController {
             self.tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
         ])
     }
-
+    
 }
 
 // MARK: - TableView Functions
 extension HomeController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.coins.count
+        return self.viewModel.coins.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -65,7 +96,7 @@ extension HomeController: UITableViewDelegate, UITableViewDataSource {
             fatalError("Coin Cell 등록 오류")
         }
         
-        let coin = self.coins[indexPath.row]
+        let coin = self.viewModel.coins[indexPath.row]
         cell.configure(with: coin)
         
         return cell
@@ -78,34 +109,9 @@ extension HomeController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         self.tableView.deselectRow(at: indexPath, animated: true)
         
-        let coin = self.coins[indexPath.row]
+        let coin = self.viewModel.coins[indexPath.row]
         let vm = ViewCryptoControllerViewModel(coin: coin)
         let vc = ViewCryptoController(vm)
         self.navigationController?.pushViewController(vc, animated: true)
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// MARK: - Variables
-
-
-// MARK: - UI Components
-
-
-// MARK: - UI Lifecycle
-
-
-// MARK: - UI Setup
--- iCrypto/iCrypto/Models/CoinError.swift
+++ iCrypto/iCrypto/Models/CoinError.swift
@@ -7,21 +7,16 @@
 
 import Foundation
 
+struct CoinStatus: Decodable {
+    let status: CoinError
+}
+
 struct CoinError: Decodable {
     let errorCode: Int
     let errorMessage: String
     
     enum CodingKeys: String, CodingKey {
-        case status
         case errorCode = "error_code"
         case errorMessage = "error_message"
-    }
-    
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        let status = try container.nestedContainer(keyedBy: CodingKeys.self, forKey: .status)
-        
-        errorCode = try status.decode(Int.self, forKey: .errorCode)
-        errorMessage = try status.decode(String.self, forKey: .errorMessage)
     }
 }
