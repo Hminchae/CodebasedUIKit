51e072bf03a59b4bb271ec074b609acb991a6f8c
Hminchae <hminchae@gmail.com>
2024-07-11 00:43:02 +0900

    iCrypto App :  모델 수정

--- iCrypto/iCrypto/Models/Coin.swift
+++ iCrypto/iCrypto/Models/Coin.swift
@@ -7,36 +7,35 @@
 
 import Foundation
 
-struct Coin {
-    
+struct CoinArray: Decodable {
+    let data: [Coin]
+}
+
+struct Coin: Decodable {
     let id: Int
     let name: String
-    let max_supply: Int?
-    let cmc_rank: Int
-    let quote: Quote
+    let maxSupply: Int?
+    let rank: Int
+    let pricingData: PricingData
     
     var logoURL: URL? {
-        return URL(string: "https://s2.coinmarketcap.com/static/img/coins/200x200/1.png")
+        return URL(string: "https://s2.coinmarketcap.com/static/img/coins/200x200/\(id).png")
     }
     
-    struct Quote {
-        let CAD: CAD
-        
-        struct CAD {
-            let price: Double
-            let market_cap: Double
-        }
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case name = "name"
+        case maxSupply = "max_supply"
+        case rank = "cmc_rank"
+        case pricingData = "quote"
     }
 }
 
-extension Coin {
-    
-    public static func getMockArray() -> [Coin] {
-        return [
-            Coin(id: 1, name: "비트코인", max_supply: 200, cmc_rank: 1, quote: Quote(CAD: Quote.CAD(price: 50000, market_cap: 1_000_000))),
-            Coin(id: 1, name: "이더리움", max_supply: 200, cmc_rank: 2, quote: Quote(CAD: Quote.CAD(price: 50000, market_cap: 1_000_000))),
-            Coin(id: 1, name: "이더리움 클래식", max_supply: 200, cmc_rank: 3, quote: Quote(CAD: Quote.CAD(price: 50000, market_cap: 1_000_000)))
-            
-        ]
-    }
+struct PricingData: Decodable {
+    let CAD: CAD
+}
+
+struct CAD: Decodable {
+    let price: Double
+    let market_cap: Double
 }
---
+++ iCrypto/iCrypto/Models/CoinError.swift
@@ -0,0 +1,27 @@
+//
+//  CoinError.swift
+//  iCrypto
+//
+//  Created by 황민채 on 7/10/24.
+//
+
+import Foundation
+
+struct CoinError: Decodable {
+    let errorCode: Int
+    let errorMessage: String
+    
+    enum CodingKeys: String, CodingKey {
+        case status
+        case errorCode = "error_code"
+        case errorMessage = "error_message"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let status = try container.nestedContainer(keyedBy: CodingKeys.self, forKey: .status)
+        
+        errorCode = try status.decode(Int.self, forKey: .errorCode)
+        errorMessage = try status.decode(String.self, forKey: .errorMessage)
+    }
+}
