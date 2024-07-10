a4136a3c7e4edfe76b717a5e3cdfcb468b3e682b
Hminchae <hminchae@gmail.com>
2024-07-10 17:58:09 +0900

    iCrypto App: 시드파일 생성

---
+++ iCrypto/iCrypto/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  iCrypto
+//
+//  Created by 황민채 on 7/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+}
+
