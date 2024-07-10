e5360a84a2ce70df13fff16d53f106241d8a0656
Hminchae <hminchae@gmail.com>
2024-07-10 17:56:42 +0900

    Day 36 Assignment : WeatherMVVM 시드파일 생성

---
+++ WeatherMVVM/WeatherMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherMVVM
+//
+//  Created by 황민채 on 7/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
