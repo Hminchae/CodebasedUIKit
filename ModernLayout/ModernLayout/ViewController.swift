bbec631dce2119250ea782284fff88a0d4eb7a22
Hminchae <hminchae@gmail.com>
2024-07-13 02:08:02 +0900

    ModernApp : 시드파일 생성

---
+++ ModernLayout/ModernLayout/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ModernLayout
+//
+//  Created by 황민채 on 7/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
